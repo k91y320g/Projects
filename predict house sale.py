6651b500e2eb3b466c6fc0f5464cf579c5ada2fa
k91y320g <72641915+k91y320g@users.noreply.github.com>
2020-11-10 19:03:50 -0800

    Add files via upload

---
+++ predict house sale.py
@@ -0,0 +1,209 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+import seaborn as sns
+
+from scipy import stats
+from scipy.stats import  norm
+from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.filterwarnings('ignore')
+
+data_train = pd.read_csv("c:/users/kevin/desktop/train.csv")
+#print(data_train)
+
+#print(data_train['SalePrice'].describe())
+
+sns.distplot(data_train['SalePrice'])
+#plt.show()
+
+#skewness and kurtosis
+print("Skewness: %f" % data_train['SalePrice'].skew())
+print("Kurtosis: %f" % data_train['SalePrice'].kurt())
+
+#类别型特征
+
+# CentralAir
+var = 'CentralAir'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+#plt.show()
+
+# OverallQual
+var = 'OverallQual'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+#plt.show()
+
+# YearBuilt boxplot
+var = 'YearBuilt'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(26, 12))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+#plt.show()
+
+# YearBuilt  scatter
+var = 'YearBuilt'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+data.plot.scatter(x=var, y="SalePrice", ylim=(0, 800000))
+#plt.show()
+
+# Neighborhood
+var = 'Neighborhood'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(26, 12))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+#plt.show()
+
+
+#数值型特征
+
+#LotArea
+var  = 'LotArea'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
+#plt.show()
+
+#GrLivArea
+var  = 'GrLivArea'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
+#plt.show()
+
+#TotalBsmtSF
+var  = 'TotalBsmtSF'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
+#plt.show()
+
+#MiscVal
+var  = 'MiscVal'
+data = pd.concat([data_train['SalePrice'], data_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
+#plt.show()
+
+#GarageArea/GarageCars
+var  = ['GarageArea', 'GarageCars']
+for index in range(2):
+    data = pd.concat([data_train['SalePrice'], data_train[var[index]]], axis=1)
+    data.plot.scatter(x=var[index], y='SalePrice', ylim=(0, 800000))
+#plt.show()
+
+#关系矩阵
+corrmat = data_train.corr()
+f, ax = plt.subplots(figsize=(20, 9))
+sns.heatmap(corrmat, vmax=0.8, square=True)
+#plt.show()
+
+from sklearn import preprocessing
+f_names = ['CentralAir', 'Neighborhood']
+for x in f_names:
+    label = preprocessing.LabelEncoder()
+    data_train[x] = label.fit_transform(data_train[x])
+corrmat = data_train.corr()
+f, ax = plt.subplots(figsize=(20, 9))
+sns.heatmap(corrmat, vmax=0.8, square=True)
+#plt.show()
+
+#房价关系矩阵
+k  = 10 # 关系矩阵中将显示10个特征
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(data_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, \
+                 square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+#plt.show()
+
+#绘制关系点图
+sns.set()
+cols = ['SalePrice','OverallQual','GrLivArea', 'GarageCars','TotalBsmtSF', 'FullBath', 'TotRmsAbvGrd', 'YearBuilt']
+sns.pairplot(data_train[cols], size = 2.5)
+#plt.show()
+
+#开始模拟数据
+from sklearn import preprocessing
+from sklearn import linear_model, svm, gaussian_process
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.cross_validation import train_test_split
+import numpy as np
+
+
+# 获取数据
+cols = ['OverallQual','GrLivArea', 'GarageCars','TotalBsmtSF', 'FullBath', 'TotRmsAbvGrd', 'YearBuilt']
+x = data_train[cols].values
+y = data_train['SalePrice'].values
+x_scaled = preprocessing.StandardScaler().fit_transform(x)
+y_scaled = preprocessing.StandardScaler().fit_transform(y.reshape(-1,1))
+X_train,X_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=0.33, random_state=42)
+
+clfs = {
+        'svm':svm.SVR(),
+        'RandomForestRegressor':RandomForestRegressor(n_estimators=400),
+        'BayesianRidge':linear_model.BayesianRidge()
+       }
+for clf in clfs:
+    try:
+        clfs[clf].fit(X_train, y_train)
+        y_pred = clfs[clf].predict(X_test)
+        print(clf + " cost:" + str(np.sum(y_pred-y_test)/len(y_pred)) )
+    except Exception as e:
+        print(clf + " Error:")
+        print(str(e))
+
+#由上面结果选择随机森林回归算法
+
+cols = ['OverallQual','GrLivArea', 'GarageCars','TotalBsmtSF', 'FullBath', 'TotRmsAbvGrd', 'YearBuilt']
+x = data_train[cols].values
+y = data_train['SalePrice'].values
+X_train,X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+clf = RandomForestRegressor(n_estimators=400)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print(y_pred)
+
+# 保存clf，共下面计算测试集数据使用
+# rfr = clf
+
+#检验测试集数据
+
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+rfr = clf
+
+data_test = pd.read_csv("c:/users/kevin/desktop/test.csv")
+
+data_test = pd.read_csv("c:/users/kevin/desktop/test.csv")
+print(data_test[cols].isnull().sum())
+
+print(data_test[cols].isnull().sum())
+
+print(data_test['GarageCars'].describe())
+print(data_test['TotalBsmtSF'].describe())
+
+cols2 = ['OverallQual','GrLivArea', 'FullBath', 'TotRmsAbvGrd', 'YearBuilt']
+cars = data_test['GarageCars'].fillna(1.766118)
+bsmt = data_test['TotalBsmtSF'].fillna(1046.117970)
+data_test_x = pd.concat( [data_test[cols2], cars, bsmt] ,axis=1)
+print(data_test_x.isnull().sum())
+
+x = data_test_x.values
+y_te_pred = rfr.predict(x)
+print(y_te_pred)
+
+print(y_te_pred.shape)
+print(x.shape)
+
+prediction = pd.DataFrame(y_te_pred, columns=['SalePrice'])
+result = pd.concat([ data_test['Id'], prediction], axis=1)
+# result = result.drop(resultlt.columns[0], 1)
+print(result.columns)
+
+# 保存预测结果
+result.to_csv('c:/users/kevin/desktop/Predictions.csv', index=False)
